25713e4407f15451dbe6660950965f34b665970c
Rian <rian.sj@gmail.com>
2017-12-06 14:30:35 +0000

    add name route

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,14 @@
 module MoviesHelper
+    def format_total_gross(movie)
+        if movie.flop?
+            # HTML strings returned from helper methods will automatically be escaped by default to prevent cross-site scripting. 
+            # Strings returned from helpers that should not be escaped (e.g. strings containing HTML tags) must be marked as HTML-safe 
+            # by calling the html_safe method on the string. 
+            # Alternatively, you can use the content_tag helper which always returns an HTML-safe string with the content surrounded by the tag.
+           content_tag(:strong, 'Flop!' )
+           # this also work: "<strong>Flop!</strong>".html_safe
+        else
+            number_to_currency(movie.total_gross)
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root "movies#index"
+  
   # verb "url" => "name_of_controllers#name_of_action"
   get "movies" => "movies#index"
+  
+  # Variables in routes start with a colon (:)
+  # All URL request parameters are automatically captured by Rails in a Ruby hash called params. 
+  # You can access this hash inside of any controller action. 
+  # In this case, because the route has a variable named :id, you can get the movie id that's embedded in the URL by accessing params[:id].
+  get "movies/:id" => "movies#show", as: "movie"
 end
